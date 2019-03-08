cc14ff1fcce484a4e938ba477f087e53d25a6e2c
Ric Harvey <ric@digilution.io>
2019-03-08 22:40:37 +0000

    enable ci/cd

--- get_tags.sh
+++ get_tags.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   echo "Getting all releases"
-  curl https://api.github.com/repos/aws/aws-cli/tags | jq -r .[].name > releases
-  curl https://api.github.com/repos/aws/aws-cli/tags | jq -r .[].name | head -n1 > latest
+  curl https://api.github.com/repos/aws/aws-cli/tags | jq -r .[].name | grep -v dev > releases
+  cat releases | head -n1 > latest
 
   curl https://api.github.com/repos/awslabs/aws-shell/tags | jq -r .[].name | grep -v reinvent-demo > shell-releases
-  curl https://api.github.com/repos/awslabs/aws-shell/tags | jq -r .[].name | grep -v reinvent-demo | head -n1 > shell-latest
+  cat shell-releases | head -n1 > shell-latest
