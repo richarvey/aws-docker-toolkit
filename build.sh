d5e020ed1f8b11370bb404c2aa1520b516b3e773
Ric Harvey <ric@digilution.io>
2019-05-07 20:27:37 +0100

    roll back buildx for now

--- build.sh
+++ build.sh
@@ -11,27 +11,16 @@ if [ ${latest} == "200" ]; then
 else
     echo "Building: awscli"
     docker version
-    docker buildx ls
-    docker buildx create --name aws-toolkit-builder
-    docker use aws-toolkit-builder
-#    docker build --build-arg CLI_VERSION="${VERSION}" --build-arg SHELL_VERSION="${SHELL_VERSION}" --build-arg CDK_VERSION="${CDK_VERSION}" -t "richarvey/awscli:${VERSION}" . && \ 
-#    docker push "richarvey/awscli:${VERSION}"
+    docker build --build-arg CLI_VERSION="${VERSION}" --build-arg SHELL_VERSION="${SHELL_VERSION}" --build-arg CDK_VERSION="${CDK_VERSION}" -t "richarvey/awscli:${VERSION}" . && \ 
+    docker push "richarvey/awscli:${VERSION}"
 
-    docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg CLI_VERSION="${VERSION}" --build-arg SHELL_VERSION="${SHELL_VERSION}" --build-arg CDK_VERSION="${CDK_VERSION}" -t richarvey/awscli:latest --push .
-
-    docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg CLI_VERSION="${VERSION}" --build-arg SHELL_VERSION="${SHELL_VERSION}" --build-arg CDK_VERSION="${CDK_VERSION}" -t richarvey/awscli:${VERSION} --push .
-
-    #docker tag richarvey/awscli:${VERSION} richarvey/awscli:latest && docker push "richarvey/awscli:latest"
+    docker tag richarvey/awscli:${VERSION} richarvey/awscli:latest && docker push "richarvey/awscli:latest"
 
 # Build Slim
-#    echo "Building: awscli:slim"
-#    docker build --build-arg CLI_VERSION="${VERSION}" -t "richarvey/awscli:${VERSION}-slim" -f Dockerfile-slim . && \
-#    docker push "richarvey/awscli:${VERSION}-slim"
+    echo "Building: awscli:slim"
+    docker build --build-arg CLI_VERSION="${VERSION}" -t "richarvey/awscli:${VERSION}-slim" -f Dockerfile-slim . && \
+    docker push "richarvey/awscli:${VERSION}-slim"
 
-#    docker tag richarvey/awscli:${VERSION}-slim richarvey/awscli:slim && docker push "richarvey/awscli:slim"
-
-    docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg CLI_VERSION="${VERSION}" -t richarvey/awscli:slim --push .
-
-    docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg CLI_VERSION="${VERSION}" -t richarvey/awscli:${VERSION}slim --push .
+    docker tag richarvey/awscli:${VERSION}-slim richarvey/awscli:slim && docker push "richarvey/awscli:slim"
 
 fi
