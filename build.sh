adcfbb4f418a826cc68cf8d2bbcc43c3dde10d5d
Ric Harvey <ric@digilution.io>
2019-05-07 19:54:37 +0100

    try multiarch build

--- build.sh
+++ build.sh
@@ -13,12 +13,21 @@ else
     docker build --build-arg CLI_VERSION="${VERSION}" --build-arg SHELL_VERSION="${SHELL_VERSION}" --build-arg CDK_VERSION="${CDK_VERSION}" -t "richarvey/awscli:${VERSION}" . && \ 
     docker push "richarvey/awscli:${VERSION}"
 
-    docker tag richarvey/awscli:${VERSION} richarvey/awscli:latest && docker push "richarvey/awscli:latest"
+    docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg CLI_VERSION="${VERSION}" --build-arg SHELL_VERSION="${SHELL_VERSION}" --build-arg CDK_VERSION="${CDK_VERSION}" -t richarvey/awscli:latest --push .
+
+    docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg CLI_VERSION="${VERSION}" --build-arg SHELL_VERSION="${SHELL_VERSION}" --build-arg CDK_VERSION="${CDK_VERSION}" -t richarvey/awscli:${VERSION} --push .
+
+    #docker tag richarvey/awscli:${VERSION} richarvey/awscli:latest && docker push "richarvey/awscli:latest"
 
 # Build Slim
-    echo "Building: awscli:slim"
-    docker build --build-arg CLI_VERSION="${VERSION}" -t "richarvey/awscli:${VERSION}-slim" -f Dockerfile-slim . && \
-    docker push "richarvey/awscli:${VERSION}-slim"
+#    echo "Building: awscli:slim"
+#    docker build --build-arg CLI_VERSION="${VERSION}" -t "richarvey/awscli:${VERSION}-slim" -f Dockerfile-slim . && \
+#    docker push "richarvey/awscli:${VERSION}-slim"
 
-    docker tag richarvey/awscli:${VERSION}-slim richarvey/awscli:slim && docker push "richarvey/awscli:slim"
+#    docker tag richarvey/awscli:${VERSION}-slim richarvey/awscli:slim && docker push "richarvey/awscli:slim"
+
+    docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg CLI_VERSION="${VERSION}" -t richarvey/awscli:slim --push .
+
+    docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg CLI_VERSION="${VERSION}" -t richarvey/awscli:${VERSION}slim --push .
+
 fi
